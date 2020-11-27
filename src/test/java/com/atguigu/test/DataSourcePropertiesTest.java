04c618eff70c56fd02b4334ef12a164810a5c47f
git-wyy <1215243760@qq.com>
2020-11-27 09:57:40 +0800

    fix dev

--- src/test/java/com/atguigu/test/DataSourcePropertiesTest.java
+++ src/test/java/com/atguigu/test/DataSourcePropertiesTest.java
@@ -3,6 +3,6 @@ package com.atguigu.test;
 public class DataSourcePropertiesTest {
 
     public static void main(String[] args) {
-        System.out.println("master");
+        System.out.println("dev");
     }
 }
